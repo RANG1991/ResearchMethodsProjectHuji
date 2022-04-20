ba3a0b154a835d4ca760f2b9d9189742f1e0abaf
Ran Galun <galun.ran@gmail.com>
2022-04-20 13:25:14 +0300

    finishing scraping script

--- githubscraping.py
+++ githubscraping.py
@@ -29,23 +29,34 @@ def login(login_url, driver, username, password):
     sleep(3)
 
 
-def get_repository_props(repository_name):
-
-
-def get_all_repositories_props_in_page(driver):
+def get_all_repositories_props_in_page(driver, repos_props, main_url):
+    repos_elements = driver.find_elements_by_xpath('//li[@class="repo-list-item hx_hit-repo d-flex '
+                                                   'flex-justify-start py-4 public source"]')
+    for element in repos_elements:
+        repo_full_name = element.find_element_by_xpath('//a[@class="v-align-middle"]').text
+        repo_link = element.find_element_by_xpath('//a[@class="v-align-middle"]').get_attribute("href")
+        repo_num_stars = element.find_element_by_xpath('//a[@class="Link--muted"]').text
+        repos_props[repo_full_name] = (repo_link, repo_num_stars)
 
 
 def main():
     repos_props = {}
     options = Options()
     options.add_argument("--disable-notifications")
+    options.headless = True
     username, password, driver_path = read_local_config_file("./config.json")
     driver = webdriver.Chrome(driver_path, chrome_options=options)
     login("https://github.com/login", driver, username, password)
     main_url = 'https://github.com/'
-    num_pages = 100
-    for page_index in range(num_pages + 1):
-        driver.get(main_url + 'search?l=Python&o=desc&p=' + str(page_index) + '&q=Python&s=stars&type=Repositories')
+    num_pages = 10
+    for page_index in range(num_pages):
+        driver.get(main_url + 'search?l=Python&o=desc&p=' + str(page_index + 1) +
+                   '&q=Python&s=stars&type=Repositories')
         sleep(3)
         print("Item searched")
-        get_all_repositories_props_in_page(driver)
+        get_all_repositories_props_in_page(driver, repos_props, main_url)
+    print(repos_props)
+
+
+if __name__ == "__main__":
+    main()
