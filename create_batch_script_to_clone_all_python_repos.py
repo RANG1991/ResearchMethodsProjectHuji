ad81de747bd35b521fce933eb17dd1abd4642e21
Ran Galun <galun.ran@gmail.com>
2022-04-21 07:50:07 +0300

    changing cloning script

--- create_batch_script_to_clone_all_python_repos.py
+++ create_batch_script_to_clone_all_python_repos.py
@@ -3,12 +3,11 @@ import pandas as pd
 
 def main():
     df_repos = pd.read_csv("./repos_props.csv")
-    df_repos["repo_is_forked"] = df_repos["repo_is_forked"].map({'TRUE': True, 'FALSE': False})
-    df_repos = df_repos[df_repos["repo_is_forked"] is False]
+    df_repos = df_repos[df_repos["repo_is_forked"] == False]
     df_repos_links = df_repos["repo_link"].tolist()
-    repos_links = " ".join(df_repos_links)
-    with open("./clone_all_python_repos.bat", "w") as f:
-        f.write("git clone {}".format(repos_links))
+    with open("../pythonReposForMethods/clone_all_python_repos.sh", "w") as f:
+        for i in range(100):
+            f.write("git clone {}\n".format(df_repos_links[i]))
 
 
 if __name__ == '__main__':
